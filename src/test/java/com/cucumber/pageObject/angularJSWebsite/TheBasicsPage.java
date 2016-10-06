561e86d602bb7c9b4db8933e6f8916a235a69c6b
Gregory Dombchik <gbdjobs1@gmail.com>
2016-10-06 16:58:46 -0400

    Accidentally checked in local parameters.properties

--- src/test/java/com/cucumber/pageObject/angularJSWebsite/TheBasicsPage.java
+++ src/test/java/com/cucumber/pageObject/angularJSWebsite/TheBasicsPage.java
@@ -1,9 +1,20 @@
 package com.cucumber.pageObject.angularJSWebsite;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class TheBasicsPage extends AbstractPage {
+	private WebElement name;
+	
 	public TheBasicsPage(WebDriver driver){
 		super(driver);
+	}
+
+	public WebElement getName() {
+		return name;
+	}
+
+	public void setName(WebElement name) {
+		this.name = name;
 	}
 }
