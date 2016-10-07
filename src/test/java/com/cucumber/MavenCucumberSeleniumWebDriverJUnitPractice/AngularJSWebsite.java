648478c96d18a0adf1229bb39b288745d4c19a88
Gregory Dombchik <gbdjobs1@gmail.com>
2016-10-07 14:25:44 -0400

    Accidentally added commented out Steps

--- src/test/java/com/cucumber/MavenCucumberSeleniumWebDriverJUnitPractice/AngularJSWebsite.java
+++ src/test/java/com/cucumber/MavenCucumberSeleniumWebDriverJUnitPractice/AngularJSWebsite.java
@@ -44,14 +44,14 @@ public class AngularJSWebsite extends AbstractPageStepDefinition{
 
 	@Then("^I click on the Download AngularJS One button\\.$")
 	public void iClickOnTheDownloadAngularJSOneButton() throws Throwable {
-		//downloadAngularJSOnePage = landingPage.navigateToDownloadAngularJSOnePage();
+		downloadAngularJSOnePage = landingPage.navigateToDownloadAngularJSOnePage();
 	}
 
 	@Then("^I check the properties of the Download AngularJS One page\\.$")
 	public void iCheckThePropertiesOfTheDownloadAngularJSOnePage(DataTable table) throws Throwable {
 		List<List<String>> tableList = table.raw();
 		
-		/*Assert.assertTrue(downloadAngularJSOnePage.getTitleLabel().getText().equals(tableList.get(1).get(1))); //Download AngularJS
+		Assert.assertTrue(downloadAngularJSOnePage.getTitleLabel().getText().equals(tableList.get(1).get(1))); //Download AngularJS
 		Assert.assertTrue(downloadAngularJSOnePage.getBranch().getText().equals(tableList.get(2).get(1)));  //1.5.x (stable)
 		Assert.assertTrue(downloadAngularJSOnePage.getBuildMinified().getText().equals(tableList.get(3).get(1))); //Minified
 		Assert.assertTrue(downloadAngularJSOnePage.getBuildZip().getText().equals(tableList.get(4).get(1))); //Zip		
@@ -63,12 +63,12 @@ public class AngularJSWebsite extends AbstractPageStepDefinition{
 		Assert.assertTrue(downloadAngularJSOnePage.getPreviousVersions().getText().equals(tableList.get(10).get(1))); //Previous Versions
 		Assert.assertTrue(downloadAngularJSOnePage.getDownloadButton().getAttribute("href").contains(tableList.get(11).get(1)));  //angular.min.js
 		downloadAngularJSOnePageCloseButton = downloadAngularJSOnePage.getCloseButton();
-		Assert.assertTrue(downloadAngularJSOnePageCloseButton.getText().equals(tableList.get(12).get(1))); //×*/
+		Assert.assertTrue(downloadAngularJSOnePageCloseButton.getText().equals(tableList.get(12).get(1))); //×
 	}
 
 	@Then("^I click on the Close button of the Download AngularJS One page\\.$")
 	public void iClickOnTheCloseButtonOfTheDownloadAngularJSOnePage() throws Throwable {
-		//downloadAngularJSOnePageCloseButton.click();
+		downloadAngularJSOnePageCloseButton.click();
 	}
 
 	@When("^I fill in the name\\.$")
